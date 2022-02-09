a6f0caf0863314d85fa5a60bc1f77864fb752df6
Sony Philip <sony.philip@nov.com>
2022-02-09 10:21:42 -0600

    solution

---
+++ Utopian-Tree/Utopian-Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Utopian_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GrowTree(0);
+            GrowTree(1);
+            GrowTree(4);
+
+            GrowTree(6);
+            GrowTree(5);
+            GrowTree(4);
+        }
+        static void GrowTree(int n)
+        {
+            var height = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    height = height + 1;
+                }
+                else
+                {
+                    height = height * 2;
+                }
+            }
+            Console.WriteLine(height);
+        }
+    }
+}
