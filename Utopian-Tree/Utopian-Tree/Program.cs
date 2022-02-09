d0a903a6cb032a7e91e61e3688e99c9ac5b64553
lagunasj <Johnny.Lagunas@nov.com>
2022-02-09 10:08:23 -0600

    utopian tree solution

---
+++ Utopian-Tree/Utopian-Tree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utopian_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = 0;
+            int height = 1;
+
+            int n = 10;
+            for(int i=0; i<n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    height *= 2;
+                }
+                else
+                {
+                    height += 1;
+                }
+            }
+            Console.WriteLine(height);
+            Console.ReadKey();
+
+        }
+    }
+}
